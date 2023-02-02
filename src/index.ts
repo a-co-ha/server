a7f3f29c1767d032db7571e7493c0f37fac93551
seungha <frazzle124@gmail.com>
2023-02-02 16:37:50 +0900

    typeorm connection

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,19 @@ import compression from "compression";
 import cors from "cors";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { port } from "./config";
+import {
+  port,
+  TYPEORM_CONNECTION,
+  TYPEORM_HOST,
+  TYPEORM_USERNAME,
+  TYPEORM_PASSWORD,
+  TYPEORM_DATABASE,
+  TYPEORM_PORT,
+  TYPEORM_SYNCHRONIZE,
+  TYPEORM_LOGGING,
+  TYPEORM_ENTITIES,
+} from "./config";
+import { DataSource } from "typeorm";
 import resolvers from "./graphql/resolver";
 import fs from "fs";
 
@@ -14,16 +26,6 @@ const app = express();
 app.use("*", cors());
 app.use(compression());
 
-// const typeDefs = `
-//     type Query{
-//         totalPosts: Int!
-//     }
-// `;
-// const resolvers = {
-//   Query: {
-//     totalPosts: () => 100,
-//   },
-// };
 let apolloServer: any = null;
 async function startServer() {
   apolloServer = new ApolloServer({
@@ -35,6 +37,26 @@ async function startServer() {
   apolloServer.applyMiddleware({ app });
 }
 startServer();
+
+async function initialize() {
+  try {
+    const myDataSource = new DataSource({
+      type: TYPEORM_CONNECTION as "mysql",
+      host: TYPEORM_HOST,
+      port: TYPEORM_PORT,
+      username: TYPEORM_USERNAME,
+      password: TYPEORM_PASSWORD,
+      database: TYPEORM_DATABASE,
+    });
+    myDataSource.initialize().then(() => {
+      console.log("Data Source has been initialized!");
+    });
+    console.log("DB Connected!");
+  } catch (e) {
+    console.log(e);
+  }
+}
+initialize();
 
 app.listen(port, () =>
   console.log(`🚀 http://localhost:${port}${apolloServer.graphqlPath}`)
