712a1d48f4efe34cf3f8b0bd542fe03b2ec995fc
yisuho <yeek0620@gmail.com>
2023-02-04 17:15:40 +0900

    feat:post Schema

---
+++ src/schema/postSchema.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    blocks: [
+      {
+        tag: {
+          type: String,
+          required: true,
+        },
+        html: {
+          type: String,
+          required: false,
+        },
+        imageUrl: {
+          type: String,
+          required: false,
+        },
+      },
+    ],
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export { postSchema };
