21834cd7cc054699170a6e20ee038ad0204b81f6
seungha <frazzle124@gmail.com>
2023-03-03 21:24:24 +0900

    dev cloudtype

--- src/routers/oauthRouter.ts
+++ src/routers/oauthRouter.ts
@@ -5,7 +5,7 @@ import { asyncHandler } from "../utils";
 
 export const oauthRouter = express.Router();
 
-oauthRouter.get("/github", passport.authenticate("github"));
+oauthRouter.post("/github", passport.authenticate("github"));
 oauthRouter.get(
   "/github/callback",
   passport.authenticate("github", { failureRedirect: "/login" }),
