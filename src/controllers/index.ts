a1d1acf6cc18d044cc03713d5ebc3f76dd2267a0
seungha <frazzle124@gmail.com>
2023-03-23 08:27:10 +0000

    충돌수정

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,8 +3,5 @@ export * from "./pageController";
 export * from "./templateController";
 export * from "./channelController";
 export * from "./messageController";
-<<<<<<< HEAD
 export * from "./listController";
-=======
 export * from "./githubController";
->>>>>>> feature
