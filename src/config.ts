4bc9bb936ab706584a8b0971038ea2515a789471
seungha <frazzle124@gmail.com>
2023-04-12 15:15:57 +0900

    fix : cookie옵션 수정

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,9 @@ export const sessionConfig = {
   }),
   cookie: {
     httpOnly: true, // 자바스크립트를 통해 세션 쿠키를 사용할 수 없도록 함 localhost, ip일때는 쓰면 안된다. 저장안됨
-    sameSite: "lax",
+    sameSite: "none",
+    secure: true,
+    domain: ".acoha.store",
   },
   credentials: true,
   secret: SESSION_SECRET,
